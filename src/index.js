efc574109d32e26e0fa1958066e8b5a16a9f96d1
amit-sahani <amitg.one19@gmail.com>
2021-02-06 19:57:33 +0530

    app which show  wether it is summer or winter season based on location and month

--- src/index.js
+++ src/index.js
@@ -1,17 +1,87 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay';
+import "semantic-ui-css/semantic.min.css";
+import Spinner from './Spinner';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const App = () => {
+//     async function getGeoLoc(){
+//         let loc = await new Promise((resolve, reject) =>  window.navigator.geolocation.getCurrentPosition(resolve, reject));
+//         console.log(loc );
+//     }
+//     try {
+//         getGeoLoc();
+//     } catch (error) {
+//         console.log(error);
+//     }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+//     return (
+//         <div>Latitude: </div>
+//     );
+// };
+
+async function getGeoLoc(obj) {
+    try {
+        let loc = await new Promise((resolve, reject) => window.navigator.geolocation.getCurrentPosition(resolve, reject));
+        console.log(loc);
+        obj.setState({ lat: loc.coords.latitude })
+    } catch (error) {
+        obj.setState({errorMessage: error.message});
+        console.log("This is error => " + error);
+    }
+
+}
+
+class App extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { lat: null, errorMessage: "" };
+    //     // window.navigator.geolocation.getCurrentPosition(
+    //     //     position => { this.setState({lat: position.coords.latitude}) },
+    //     //     err => console.log(err)
+    //     // );
+
+    // }
+
+    state = { lat: null, errorMessage: ""}
+
+    componentDidMount(){
+        getGeoLoc(this);
+
+    }
+
+    renderContent(){
+        if(this.state.lat && !this.state.errorMessage ){
+            return <SeasonDisplay {...this.state} />
+            //<div><b>Location: </b>{ this.state.lat}</div>
+        }
+        if(!this.state.lat && this.state.errorMessage ){
+            return <div><b>Error: </b> { this.state.errorMessage}</div>
+        }
+        
+        return <Spinner message="Please select the location." />
+ 
+    };
+
+
+    render() {
+
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
+
+        // return (
+        //     <div>{ this.state.lat ?
+        //         ( <>Latitude: {this.state.lat} 
+        //         <br/> </>) :
+        //         ( <>Error: {this.state.errorMessage} </>)
+        //     }
+        //     </div>
+        // );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
